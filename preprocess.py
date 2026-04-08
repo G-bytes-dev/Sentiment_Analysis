73d5b3b3f5b8fc0244fefc8d0d84113f1a76130b
Ganesh <ganeshguna018@gmail.com>
2026-04-08 19:06:50 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-stop_words = set(stopwords.words('english'))
+stop_words = set(stopwords.words('English'))
 lemmatizer = WordNetLemmatizer()
 
 def preprocess(text):
